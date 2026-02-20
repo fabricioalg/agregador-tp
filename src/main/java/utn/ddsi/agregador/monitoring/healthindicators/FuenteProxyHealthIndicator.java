2b05c7926950fc68177558ac828e659880d2e373
Aponte Lucas <laponte@frba.utn.edu.ar>
2026-02-19 23:50:42 -0300

    intentoBorrarProxy3

--- src/main/java/utn/ddsi/agregador/monitoring/healthindicators/FuenteProxyHealthIndicator.java
+++ src/main/java/utn/ddsi/agregador/monitoring/healthindicators/FuenteProxyHealthIndicator.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.time.Duration;
 
+@Slf4j
 public class FuenteProxyHealthIndicator extends AbstractDependencyHealthIndicator {
 
     private final RestTemplate restTemplate;
