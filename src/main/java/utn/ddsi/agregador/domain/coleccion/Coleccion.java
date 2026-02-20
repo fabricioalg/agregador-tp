12541abc3d7103a728263bfa1b1e37cbdfae7673
fabricioalg <flopezgarro@frba.utn.edu.ar>
2026-02-20 19:25:53 -0300

    descomento metodos

--- src/main/java/utn/ddsi/agregador/domain/coleccion/Coleccion.java
+++ src/main/java/utn/ddsi/agregador/domain/coleccion/Coleccion.java
@@ -89,8 +89,7 @@ public class Coleccion {
 //      List<Hecho> hechosConsensuados=this.algoritmoDeConsenso.aplicar(this.hechos,fuentes);
 //        return hechosConsensuados
 //  }
-/*
-    todo esto quedó sin usar
+
     public void aplicarConsenso(HechoXColeccion hxc, EvidenciaDeHecho evidencia) {
         if (algoritmoDeConsenso.aplicar(evidencia, fuentes.size())) {
             hxc.setConsensuado(Boolean.TRUE);
@@ -125,6 +124,4 @@ public class Coleccion {
                 .map(HechoXColeccion::getHecho)
                 .collect(Collectors.toList());
     }
- */
-
 }
